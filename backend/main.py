14c78290f91b8d00b0a25e552203c33d2db69785
aishna05 <160373169+aishna05@users.noreply.github.com>
2025-06-08 23:55:24 +0530

    ..

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 
-models.Base.metadata.create_all(bind=engine)
+backend.models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
